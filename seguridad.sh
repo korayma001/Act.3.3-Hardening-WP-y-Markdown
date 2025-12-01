c188f2743cae8998ac626d4d306535b324d8ce75
Korayma <kcalrod001@g.educaand.es>
2025-12-01 01:29:14 +0100

    Update seguridad.sh

--- seguridad.sh
+++ seguridad.sh
@@ -13,13 +13,10 @@ echo ""
 echo "Creando copia de seguridad......"
 # Indicamos la ruta de nuestro alojamiento.
 WP_DIR="/home/www/koraymaasir.atwebpages.com/wordpress"
-
 # Ruta y nombre de de la copia de seguridad
 BACKUP_FILE="/backupsWP/wp_backup_$(date +%Y%m%d).tar.gz"
-
 # Archivo comprimido de todo nuetsro WP
 tar -czf "$BACKUP_FILE" "$WP_DIR"
-
 # Idicamos donde se ha guardado
 echo "Backup creado en: $BACKUP_FILE"
 echo ""
@@ -27,9 +24,7 @@ echo ""
 echo "Asignando permisos de seguridad...... "
 # Asignar permisos 755 a directorios (para poder navegar)
 find "$WP_DIR" -type d -exec chmod 755 {} \;
-
 # Asignar permisos 644 a archivos (para poder leer y no escribir ni ejecutar)
 find "$WP_DIR" -type f -exec chmod 644 {} \;
-
 echo "Permiso 755 a directorios y 644 a archivos asignados"
 echo "SCRIPT FINALIZADO"
